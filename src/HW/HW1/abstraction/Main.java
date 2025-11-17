6e87959569e31a24be7d62f0ad7c398c1b757d68
max2xam2 <moskin044@gmail.com>
2025-11-17 20:30:53 +0300

    add new file

---
+++ src/HW/HW1/abstraction/Main.java
@@ -0,0 +1,15 @@
+/*package HW.HW1.abstraction;
+
+public class Main  {
+  public static void main(String[] args){
+    Vehicle newCar = new Car("BMW");
+    newCar.forces = 89;
+    Vehicle newMotorbike = new Motorbyke("Ducati", 777);
+    newCar.start();
+    newCar.brand = "Mercedes";
+    newCar.forces = 700;
+    newMotorbike.start();
+    newCar.start();
+  }
+}
+*/

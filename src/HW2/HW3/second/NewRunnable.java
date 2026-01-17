5264b790e91620745603a93c72de883602fad3f5
max2xam2 <moskin044@gmail.com>
2026-01-17 11:17:43 +0300

    add new file 17/01

--- src/HW2/HW3/second/NewRunnable.java
+++ src/HW2/HW3/second/NewRunnable.java
@@ -4,13 +4,13 @@ public class NewRunnable implements Runnable {
   @Override
   public void run() {
     for (int i = 0; i < 100; i++) {
-      if (i % 10 == 0){
+      if (i % 10 == 0) {
         System.out.println(Thread.currentThread().getName() + " " + i);
-        try {
-          Thread.sleep(2000);
-        } catch (InterruptedException e) {
-          throw new RuntimeException(e);
-        }
+      }
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
       }
     }
   }
